f32271792abcf45e96aff2ae5269d47b51e4e202
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-15 00:46:38 +0530

    Added the reducer and store for Redux react app

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+/*A Reducer is a pure function*/
+import { DISHES } from '../shared/dishes';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
+import {COMMENTS} from '../shared/comments';
+
+export const initialState = {
+    dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+};
+
+export const Reducer = (state= initialState, action) => {
+    return state;
+} 
