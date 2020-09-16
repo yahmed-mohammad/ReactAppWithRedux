4bcf28f6ee23ffdf9fef91bb316c3276cd699aaf
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-16 21:46:13 +0530

    Splitting and combining the reducers using react-redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,4 @@
+/*
 import {createStore} from 'redux';
 import { Reducer, initialState } from './reducer'
 
@@ -5,6 +6,25 @@ export const ConfigureStore = () => {
     const store = createStore(
         Reducer, // reducer
         initialState, // our initialState
+    );
+
+    return store;
+}
+*/
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
