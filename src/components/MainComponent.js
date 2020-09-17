539644bec1fd09f0ae9915699b1685ae8144913c
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-17 21:23:55 +0530

    Added a pre-loader using Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,9 +7,8 @@ import Contact from './ContactComponent';
 import DishDetail from './DishDetailsFunctionComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import About from './AboutComponent'
-import { addComment } from '../redux/ActionCreators';
-
+import About from './AboutComponent';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 
@@ -24,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 
 });
 
@@ -34,13 +34,17 @@ class Main extends Component {
     super(props);
   }
 
-  
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
 
   render() {
     const HomePage = () => {
       return(
-        <Home 
-              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          <Home 
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
               promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
               leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
@@ -48,14 +52,15 @@ class Main extends Component {
     }
 
     const DishWithId = ({match}) => {
-      console.log(this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10)));
       return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-        addComment={this.props.addComment}
-        />
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment}
+          />
       );
-    }
+    };
       
     return (
       <div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -11,11 +11,13 @@ export const ConfigureStore = () => {
     return store;
 }
 */
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -24,7 +26,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
