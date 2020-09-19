e335f6ecd1ec65d621aa43cd7a71e729c3d53515
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-19 20:56:50 +0530

    Added Hi,

--- src/components/DishDetailsFunctionComponent.js
+++ src/components/DishDetailsFunctionComponent.js
@@ -3,6 +3,7 @@ import {Card, CardImg, CardText,  CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const DishDetail = (props) => {
@@ -60,7 +61,7 @@ function RenderDish({dish}) {
     return(
         <div>
                 <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle heading>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,19 +1,23 @@
 import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
             /* State . length gives the next id based on current length */
-            comment.id = state.length;
+            comment.id = state.comments.length;
             comment.date = new Date().toISOString();
-            console.log("Comment: ", comment);
             /**
              * Concat is immutable method, which does not change the current state
              * It creates the new state and returns that
              */
-            return state.concat(comment);
+            return { ...state, comments: state.comments.concat(comment)};
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+         
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, errMess: action.payload};
 
         default:
           return state;
